bf5e3d1e6e514d811bdf50e3170421cdf6089656
Murilo Nogueira de Avila <murilo.avila068@gmail.com>
2015-07-16 14:02:46 -0300

    OverrideProjeto
    teste de override

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            databaseClass db = new databaseClass();
+            db.selecionar("clientes");
+            db.selecionar("clientes", "*");
+            db.selecionar("clientes", "nome", "WHERE id = 1");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplication1/databaseClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class databaseClass
+    {
+        public void selecionar(string tabela)
+        {
+            Console.WriteLine("Select * from " + tabela + ";");
+        }
+
+        public void selecionar(string tabela, string linha = "*")
+        {
+            Console.WriteLine("Select " + linha + " from " + tabela + ";");
+        }
+
+        public void selecionar(string tabela, string linha, string condicao)
+        {
+            Console.WriteLine("Select " + linha + " from " + tabela + " " + condicao + ";");
+        }
+    }
+}
